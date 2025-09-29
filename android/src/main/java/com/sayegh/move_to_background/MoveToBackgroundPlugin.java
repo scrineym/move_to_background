ffd70999b52c1299c6a09d9a1b86f19a054ca3db
scriney <michael.scriney@dcu.ie>
2025-09-29 16:59:17 +0100

    v2 update

--- android/src/main/java/com/sayegh/move_to_background/MoveToBackgroundPlugin.java
+++ android/src/main/java/com/sayegh/move_to_background/MoveToBackgroundPlugin.java
@@ -1,91 +1,61 @@
-package com.sayegh.move_to_background;
+package com.sayegh.movetobackground;
 
-import android.app.Activity;
-import android.content.Context;
-import android.util.Log;
-
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import android.app.Activity;
 
-/** MoveToBackgroundPlugin */
 public class MoveToBackgroundPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-  private static final String CHANNEL_NAME = "move_to_background";
   private MethodChannel channel;
-  private static Activity activity;
-
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    if (registrar.activity() != null) {
-      MoveToBackgroundPlugin.activity = registrar.activity();
-    }
-    MoveToBackgroundPlugin plugin = new MoveToBackgroundPlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
-  }
+  private Activity activity;
 
   @Override
-  @SuppressWarnings("deprecation")
-  public void onAttachedToEngine(FlutterPluginBinding binding) {
-    setupChannel(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
-  }
-
-  @Override
-  public void onDetachedFromEngine(FlutterPluginBinding binding) {
-    teardownChannel();
-  }
-
-
-  private void setupChannel(BinaryMessenger messenger, Context context) {
-    channel = new MethodChannel(messenger, CHANNEL_NAME);
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "move_to_background");
     channel.setMethodCallHandler(this);
-
-  }
-
-  private void teardownChannel() {
-    channel.setMethodCallHandler(null);
-    channel = null;
   }
 
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("moveTaskToBack")) {
-      if (MoveToBackgroundPlugin.activity != null) {
-        MoveToBackgroundPlugin.activity.moveTaskToBack(true);
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("moveToBackground")) {
+      if (activity != null) {
+        activity.moveTaskToBack(true);
+        result.success(true);
       } else {
-        Log.e("MoveToBackgroundPlugin", "moveTaskToBack failed: activity=null");
+        result.error("NO_ACTIVITY", "Activity is null", null);
       }
-      result.success(true);
     } else {
       result.notImplemented();
     }
   }
 
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
 
   @Override
-  public void onAttachedToActivity(ActivityPluginBinding binding)
-  {
-    MoveToBackgroundPlugin.activity = binding.getActivity();
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    activity = binding.getActivity();
   }
 
   @Override
   public void onDetachedFromActivityForConfigChanges() {
-    MoveToBackgroundPlugin.activity = null;
+    activity = null;
   }
 
   @Override
-  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
-    MoveToBackgroundPlugin.activity = binding.getActivity();
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+    activity = binding.getActivity();
   }
 
   @Override
   public void onDetachedFromActivity() {
-    MoveToBackgroundPlugin.activity = null;
+    activity = null;
   }
-
 }
